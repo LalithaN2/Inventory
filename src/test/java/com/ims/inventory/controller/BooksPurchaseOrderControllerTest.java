e3f7b961e7cbfd77c418646d01fd94fd3a13b709
ctsjavafsd02 <ctsjavafsd02@JavaFSD117.iiht.tech>
2020-06-18 11:54:15 +0530

    import statement changed

--- src/test/java/com/ims/inventory/controller/BooksPurchaseOrderControllerTest.java
+++ src/test/java/com/ims/inventory/controller/BooksPurchaseOrderControllerTest.java
@@ -1,6 +1,7 @@
 package com.ims.inventory.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.text.ParseException;
